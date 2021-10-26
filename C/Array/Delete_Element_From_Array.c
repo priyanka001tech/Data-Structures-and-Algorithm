a75c7b2158f0be8f79cc48ddcdb28e27aa3f62fa
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2021-10-26 18:09:50 +0530

    Create Delete_Element_From_Array.c

---
+++ C/Array/Delete_Element_From_Array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    int array[100], position, c, n,s=0;
+    printf("Enter number of elements in array: ");
+    scanf("%d",&n);
+    s=n;
+    printf("Enter %d elements ", n);
+    for ( c = 0 ; c < n ; c++ )
+    scanf("%d",&array[c]);
+    printf("Enter the location where you wish to delete element\n");
+    scanf("%d",&position);
+    if ( position>=n+1||position <1){   
+    	printf("Deletion not possible.\n");
+    }
+    else{    
+        for(c=position-1;c<n-1;c++){
+        	array[c]=array[c+1];   
+			s++;
+		}
+        printf("Resultant array is: ");
+        for(c=0;c<n-1;c++) {      
+        	printf("%d ", array[c]);  
+        }
+		printf("\nNo of shifts %d ",s-n);      
+    }    
+	return 0;
+}

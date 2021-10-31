29b256f82f3b0ef22e7ada3ee2972810691f4a27
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2021-10-31 12:47:52 +0530

    Create Calculator.c

---
+++ C/Basic_Question/Calculator.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+	char ch;
+	int a,b;
+	printf("ENTER THE OPERATOR (+,-,*,/) : ");
+	scanf("%c",&ch);
+	printf("ENTER THE VALUES : ");
+	scanf("%d %d",&a,&b);
+	
+	switch(ch){
+		case '+': printf("sum is %d",a+b);
+				  break;
+		case '-': printf("subtraction is %d",a-b);
+				  break;
+		case '*': printf("mul is %d",a*b);
+				  break;
+		case '/': printf("division is %d",a/b);
+				  break;
+		default: printf("not correct");		  		  		  		  	
+			
+	}
+	
+}

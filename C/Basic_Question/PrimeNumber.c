a9c1702079c86ea566c5a9cb7e2405f04af27e69
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2021-11-02 17:53:37 +0530

    Create PrimeNumber.c

---
+++ C/Basic_Question/PrimeNumber.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+     int i, j, n, isPrime;
+    printf("Find prime numbers between 1 to : ");
+    scanf("%d", &n);
+
+    printf("All prime numbers between 1 to %d are:\n",n);
+    for(i=2; i<=n; i++){
+       isPrime = 1; 
+       for(j=2; j<=i/2; j++){
+          if(i%j==0){
+             isPrime = 0;
+              break;
+            }
+        }
+        if(isPrime==1) {
+           printf("%d ", i);
+        }
+    }
+    
+
+    return 0;
+}

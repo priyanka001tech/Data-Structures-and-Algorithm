2cae61031484135c85fec6814741af9d6da66030
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2021-11-02 17:53:05 +0530

    Create Perfect_Number.c

---
+++ C/Basic_Question/Perfect_Number.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, i,s=0,p;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    p=num;
+    for (i = 1; i < num; ++i) {
+        if (num % i == 0) {
+            s+=i;
+        }
+    }
+    if(s==p){
+        printf("%d is perfect number ",p);
+    }
+    else{
+        printf("%d not perfect number ",p);
+    }
+
+    return 0;
+}

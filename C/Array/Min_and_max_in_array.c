23f10d3fe55400385ac829bb3ef6acfd2eceb6bc
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2021-11-07 12:54:53 +0530

    Create Min_and_max_in_array.c

---
+++ C/Array/Min_and_max_in_array.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+int max(int a,int b){
+    if(a>b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
+int min(int a,int b){
+    if(a>b){
+        return b;
+    }
+    else{
+        return a;
+    }
+}
+int main()
+{
+    int mx=-100000;
+    int mn=99999999;
+    int a[10];
+    int s=0,i;
+     printf("Enter the numbers ");
+    for(i=0;i<10;i++){
+      scanf("%d",&a[i]);
+    }
+     for(i=0;i<9;i++){
+      if(a[i]>a[i+1]){
+         mx=max(mx,a[i]);
+      }
+      else{
+          mn=min(mn,a[i]);
+      }
+    }
+       printf("%d %d",mx,mn);
+    
+    return 0;
+}

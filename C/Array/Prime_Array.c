b9dc706c5301614f65f3b330c09047b725a10337
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2021-11-07 12:55:48 +0530

    Create Prime_Array.c

---
+++ C/Array/Prime_Array.c
@@ -0,0 +1,28 @@
+int main()
+{
+    int ar[100],i,n,j,counter;
+    printf("Enter the size of the array ");
+    scanf("%d",&n);
+    printf("Now enter the elements of the array ");
+    for(i=0;i<n;i++){
+        scanf("%d",&ar[i]);
+    }
+    printf(" Array is ");
+    for(i=0;i<n;i++){
+        printf("%d ",ar[i]);
+    }
+    printf("\n All the prime numbers in the array are ");
+    for(i=0;i<n;i++){
+       counter=0;
+        for(j=2;j<ar[i];j++){
+            if(ar[i]%j==0){
+               counter=1;
+               break;
+            }
+        }
+        if(counter==0){
+           printf("%d ",ar[i]);
+        }
+    }
+    return 0;
+}

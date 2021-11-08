01da9d54b4ddb35703a64cdd4e9411585325340e
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2021-11-08 12:24:54 +0530

    Create Strong_Number_In_Range.c

---
+++ C/Basic_Question/Strong_Number_In_Range.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+      int i, j, cur, ld;
+      int fact, sum;
+      for(i=1; i<=50000; i++){
+        cur = i;
+        sum = 0;
+        while(cur > 0){
+            fact = 1;
+            ld = cur % 10;
+            for( j=1; j<=ld; j++){
+                fact = fact * j;
+            }
+            sum += fact; 
+            cur /= 10;
+        }
+        if(sum == i){
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}

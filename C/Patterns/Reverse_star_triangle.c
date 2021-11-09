d4011bb9658cbce457ad6e11b1955b96f2634485
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2021-11-09 18:11:39 +0530

    Create Reverse_star_triangle.c

---
+++ C/Patterns/Reverse_star_triangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("the range is ");
+    scanf("%d",&n);
+    for(int i=n;i>=1;i--){
+        for(int j=i;j>=1;j--){
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

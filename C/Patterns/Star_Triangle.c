c16e7e0addccd44063128cd62dd170476ce19f37
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2021-11-09 18:10:49 +0530

    Create Star_Triangle.c

---
+++ C/Patterns/Star_Triangle.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the range : ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("* ");
+            
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+//Output:- 
+Enter the range : 7
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * * * 
+* * * * * * * 

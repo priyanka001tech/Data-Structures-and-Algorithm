9a0be93db4fb7d52fffa49688b70546355d6c24e
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2021-11-09 18:09:45 +0530

    Create ABC_Pattern.c

---
+++ C/Patterns/ABC_Pattern.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=65;
+    int n;
+    printf("Enter the range : ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("%c ",a);
+            
+        }
+        a++;
+        printf("\n");
+    }
+
+    return 0;
+}
+
+//Output:-
+Enter the range : 5
+A 
+B B 
+C C C 
+D D D D 
+E E E E E 

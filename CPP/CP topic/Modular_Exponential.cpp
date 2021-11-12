47feb22d11c9be93b2cc45f4a1493fd9907a63dd
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2021-11-12 14:59:13 +0530

    Update and rename CPP/Modular_Exponential.cpp to CPP/CP topic/Modular_Exponential.cpp

--- CPP/CP topic/Modular_Exponential.cpp
+++ CPP/CP topic/Modular_Exponential.cpp
@@ -1,14 +1,11 @@
 int power(long long x, unsigned int y, int p)
 {
     int res = 1;     
- 
     x = x % p; 
   
     if (x == 0) return 0; 
  
-    while (y > 0)
-    {
-        
+    while (y > 0){
         if (y & 1)
             res = (res*x) % p;
         y = y>>1; // y = y/2

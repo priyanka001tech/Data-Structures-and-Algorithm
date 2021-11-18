b709ee1856638222c36a3624569e3149e60c34e9
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2021-11-18 17:32:20 +0530

    Create count_year_days_months_seconds_hours.py

---
+++ python/basic_question/count_year_days_months_seconds_hours.py
@@ -0,0 +1,10 @@
+y = float(input("How many years? "))
+d = y * 365
+h = d * 24
+m = h * 60
+s = m * 60
+print(y, "years is:")
+print(d, "days")
+print(h, "hours")
+print(m, "minutes")
+print(s, "seconds")

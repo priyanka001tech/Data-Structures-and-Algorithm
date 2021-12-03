dc3e5f537e71599181318110c3e64a17c9a1b906
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2021-12-03 21:36:20 +0530

    Create Binary_Exponentiation.cpp

---
+++ CPP/Maths_DS/Binary_Exponentiation.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binPow(int x, int y)
+{
+    int res = 1;
+
+    while (y)
+    {
+        if (y % 2 == 1)
+            res *= x;
+
+        x *= x;
+        y /= 2;
+    }
+
+    return res;
+}
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+
+    cout << binPow(x, y);
+}

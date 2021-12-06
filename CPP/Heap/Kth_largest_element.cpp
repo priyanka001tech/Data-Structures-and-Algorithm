849307c90f5c9395cb65e2e316e3759a72043f71
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2021-12-06 17:25:02 +0530

    Create Kth_largest_element.cpp

---
+++ CPP/Heap/Kth_largest_element.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Kth largest elements
+priority_queue<int, vector<int>, greater<int>> heap; //min heap
+
+void solve(int a[], int n, int k) {
+
+	for (int i = 0; i < n; i++) {
+
+		heap.push(a[i]);
+		if (heap.size() > k) heap.pop();
+
+	}
+  
+	while (heap.size() > 0) {
+		cout << heap.top() << " ";
+		heap.pop();
+	}
+  
+}
+
+
+int main() {
+  
+	int n, k;
+	cin >> n >> k;
+	int a[n];
+	for (int i = 0; i < n; i++) cin >> a[i];
+
+	Solve(a, n, k);
+}

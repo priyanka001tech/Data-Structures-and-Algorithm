5657121a7c158ea49bb610e9c2f9a3d7f7c6c815
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2021-12-06 17:26:03 +0530

    Create Kth_closest_element.cpp

---
+++ CPP/Heap/Kth_closest_element.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// K closest numbers to x
+
+priority_queue<pair<int, int>> heap; //max heap
+
+int main() {
+  
+	int n, k;
+	cin >> n >> k;
+	int x;
+	cin >> x;
+	int a[n];
+	for (int i = 0; i < n; i++) cin >> a[i];
+
+	for (int i = 0; i < n; i++) {
+		heap.push({abs(a[i] - x), a[i]});
+		if (heap.size() > k) {
+			heap.pop();
+		}
+	}
+
+	while (heap.size() > 0) {
+		cout << heap.top().second << " ";
+		heap.pop();
+	}
+}

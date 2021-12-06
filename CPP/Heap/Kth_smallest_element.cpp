4b670d5c0dd6ab4a269dc5fa800c11f18576d4ba
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2021-12-06 17:23:54 +0530

    Create Kth_smallest_element.cpp

---
+++ CPP/Heap/Kth_smallest_element.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Kth smallest element
+priority_queue<int> heap; //max heap
+
+int Solve(int a[], int n, int k) {
+
+	for (int i = 0; i < n; i++) {
+		heap.push(a[i]);
+    
+		if (heap.size() > k) heap.pop();
+	}
+
+	return heap.top();
+}
+
+
+int main() {
+  
+	int n, k;
+	cin >> n >> k;
+	int a[n];
+	for (int i = 0; i < n; i++) cin >> a[i];
+
+	cout << Solve(a, n, k) << endl;
+}

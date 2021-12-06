7bd9f2d9ee239d8dbadefccba9076bac0b1c236e
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2021-12-06 17:28:27 +0530

    Update Kth_Sorted_Array.cpp

--- CPP/Heap/Kth_Sorted_Array.cpp
+++ CPP/Heap/Kth_Sorted_Array.cpp
@@ -3,13 +3,12 @@ using namespace std;
 
 // Sort a k-sorted array or nearly sorted array
 // Element that should be present in sorted array is within the [i-k,i+k] range
-
+ 
 priority_queue<int, vector<int>, greater<int>> heap; //min heap
 
 void solve(int a[], int n, int k) {
 
 	for (int i = 0; i < n; i++) {
-
 		heap.push(a[i]);
 		if (heap.size() > k) {
 			cout << heap.top() << " ";

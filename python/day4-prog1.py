64f5073ba5a8f87a48cbc48cfda0cb86be083731
Samannway Sil <71366418+Geeky-Sam01@users.noreply.github.com>
2021-12-10 12:50:50 +0530

    Create day4-prog1.py

---
+++ python/day4-prog1.py
@@ -0,0 +1,9 @@
+def ugly(num):
+        if num == 0:
+            return False
+        for i in [2, 3, 5]:
+            while num % i == 0:
+                num /= i
+        return num == 1
+ugly(1)
+ugly(7)

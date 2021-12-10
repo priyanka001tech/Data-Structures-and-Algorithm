62e330dc3caa250120def47024d9aef74b770a62
Samannway Sil <71366418+Geeky-Sam01@users.noreply.github.com>
2021-12-10 23:53:57 +0530

    Abundant, Deficient, Perfect numbers till 10,000

---
+++ python/day4-prog2.py
@@ -0,0 +1,18 @@
+def properdiv(n):
+    s=0
+    for i in range(1, n+1):
+       if n % i == 0:
+           s+=i
+    s=s-n
+    return s
+
+def adp(x):
+    if properdiv(x)>x:
+        print(x,'\tAbundant Number')
+    elif properdiv(x)<x:
+        print(x,'\tDeficit Number')
+    else:
+        print(x,'\tPerfect Number')
+
+for i in range(1,10001):
+    adp(i)

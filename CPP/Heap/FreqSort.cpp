de3a8161721c72183e287473d72553171d1c71ae
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2021-12-11 17:15:57 +0530

    Create FreqSort.cpp

---
+++ CPP/Heap/FreqSort.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Frequency Sort
+
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap; // min heap
+
+int main() {
+	int n, k;
+	cin >> n;
+	int a[n];
+	unordered_map<int, int> m;
+
+	for (int i = 0; i < n; i++) { cin >> a[i]; m[a[i]]++; }
+
+	for (auto i : m) {
+		heap.push({i.second, i.first});
+	}
+
+	while (heap.size() > 0) {
+		cout << heap.top().second << " ";
+		heap.pop();
+	}
+}

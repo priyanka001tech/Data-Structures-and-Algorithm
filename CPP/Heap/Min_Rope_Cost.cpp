44e8e15da04b508241061171ea52e77bc9905c2f
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2021-12-11 17:17:15 +0530

    Create Min_Rope_Cost.cpp

---
+++ CPP/Heap/Min_Rope_Cost.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Connect ropes to minimize the cost
+// Connect each rope and minimize the cost
+// Cost = sum of lengths of two ropes connected
+
+priority_queue<int, vector<int>, greater<int>> heap; // min heap
+
+int main() {
+
+	int n;
+	cin >> n;
+	int a[n];
+	for (int i = 0; i < n; i++) {
+		cin >> a[i]; // length of ropes
+		heap.push(a[i]);
+	}
+
+	int cost = 0 ;
+	while (heap.size() >= 2) {
+
+		int first = heap.top();
+		heap.pop();
+		int second = heap.top();
+		heap.pop();
+		cost += first + second;
+		heap.push(first + second);
+	}
+
+	cout << cost << endl;
+
+
+}

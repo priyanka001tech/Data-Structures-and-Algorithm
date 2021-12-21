c656daf87ac95e216b730d133364c25ff107b27b
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2021-12-21 16:46:46 +0530

    Update Maximize_minimum_dist.cpp

--- CPP/Binary_search/Maximize_minimum_dist.cpp
+++ CPP/Binary_search/Maximize_minimum_dist.cpp
@@ -5,7 +5,7 @@ bool isFeas(int mid,int arr[],int n,int k){
     int pos=arr[0],ele=1;
     
     for(int i=1;i<n;i++){
-        if(arr[i]-pos>=mid){ 
+        if(arr[i]-pos>=mid){  
             pos=arr[i];
             ele++;
             if(ele==k){

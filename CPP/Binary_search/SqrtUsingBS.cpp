6812dfad9f9b71942d9c02a51bcfaf12ff61f4d9
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-01-01 22:22:51 +0530

    Update SqrtUsingBS.cpp

--- CPP/Binary_search/SqrtUsingBS.cpp
+++ CPP/Binary_search/SqrtUsingBS.cpp
@@ -42,11 +42,6 @@ float FindSqrt(int n, int p) {
 
 int main() {
 
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#endif
-
 	int n, p;
 	cin >> n >> p;
 

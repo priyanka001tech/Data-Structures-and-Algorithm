1e6e421c2e9c4ec7681cce0096d88919d5d70fc3
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-01-29 21:45:10 +0530

    Create Climbing Stair.cpp

---
+++ CPP/Dynamic Programming/Climbing Stair.cpp
@@ -0,0 +1,47 @@
+// Tabulation:  Bottom UP
+// Time Complexity: O(N)
+// Reason: We are running a simple iterative loop
+
+// Space Complexity: O(N)
+// Reason: We are using an external array of size ‘n+1’.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int n=3;
+  vector<int> dp(n+1,-1);
+  
+  dp[0]= 1;
+  dp[1]= 1;
+  
+  for(int i=2; i<=n; i++){
+      dp[i] = dp[i-1]+ dp[i-2];
+  }
+  cout<<dp[n];
+  return 0;
+}
+
+// Sace Optimization: SC: O(1)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+  int n=3;
+  
+  int prev2 = 1;
+  int prev = 1;
+  
+  for(int i=2; i<=n; i++){
+      int cur_i = prev2+ prev;
+      prev2 = prev;
+      prev= cur_i;
+  }
+  cout<<prev;
+  return 0;
+}

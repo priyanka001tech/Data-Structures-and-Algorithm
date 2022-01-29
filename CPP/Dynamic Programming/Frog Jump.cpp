7147a5986f08fef62fd78a62d7b887de26c623cc
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-01-29 21:54:11 +0530

    Update Frog Jump.cpp

--- CPP/Dynamic Programming/Frog Jump.cpp
+++ CPP/Dynamic Programming/Frog Jump.cpp
@@ -2,7 +2,7 @@
 
 // Time Complexity: O(N)
 // Reason: The overlapping subproblems will return the answer in constant time O(1). Therefore the total number of new subproblems we solve is ‘n’. Hence total time complexity is O(N).
-
+ 
 // Space Complexity: O(N) 
 // Reason: We are using a recursion stack space(O(N)) and an array (again O(N)). Therefore total space complexity will be O(N) + O(N) ≈ O(N)
 

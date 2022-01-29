c3022ccb930cc27bfaef4547a0b3d930af128e42
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-01-29 21:49:41 +0530

    Create Frog Jump.cpp

---
+++ CPP/Dynamic Programming/Frog Jump.cpp
@@ -0,0 +1,84 @@
+// Memoization: Top Down
+
+// Time Complexity: O(N)
+// Reason: The overlapping subproblems will return the answer in constant time O(1). Therefore the total number of new subproblems we solve is ‘n’. Hence total time complexity is O(N).
+
+// Space Complexity: O(N)
+// Reason: We are using a recursion stack space(O(N)) and an array (again O(N)). Therefore total space complexity will be O(N) + O(N) ≈ O(N)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solve(int ind, vector<int>& height, vector<int>& dp){
+    if(ind==0) return 0;
+    if(dp[ind]!=-1) return dp[ind];
+    int jumpTwo = INT_MAX;
+    int jumpOne= solve(ind-1, height,dp)+ abs(height[ind]-height[ind-1]);
+    if(ind>1)
+        jumpTwo = solve(ind-2, height,dp)+ abs(height[ind]-height[ind-2]);
+    
+    return dp[ind]=min(jumpOne, jumpTwo);
+}
+
+
+int main() {
+
+  vector<int> height{30,10,60 , 10 , 60 , 50};
+  int n=height.size();
+  vector<int> dp(n,-1);
+  cout<<solve(n-1,height,dp);
+}
+
+// Tabulation: Bottom up
+// Time Complexity: O(N)
+// Reason: We are running a simple iterative loop
+
+// Space Complexity: O(N)
+// Reason: We are using an external array of size ‘n+1’.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  vector<int> height{30,10,60,10,60,50};
+  int n=height.size();
+  vector<int> dp(n,-1);
+  dp[0]=0;
+  for(int ind=1;ind<n;ind++){
+      int jumpTwo = INT_MAX;
+        int jumpOne= dp[ind-1] + abs(height[ind]-height[ind-1]);
+        if(ind>1)
+            jumpTwo = dp[ind-2] + abs(height[ind]-height[ind-2]);
+    
+        dp[ind]=min(jumpOne, jumpTwo);
+  }
+  cout<<dp[n-1];
+}
+
+// Space Optimization: SC: O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  vector<int> height{30,10,60,10,60,50};
+  int n=height.size();
+  int prev=0;
+  int prev2=0;
+  for(int i=1;i<n;i++){
+      
+      int jumpTwo = INT_MAX;
+      int jumpOne= prev + abs(height[i]-height[i-1]);
+      if(i>1)
+        jumpTwo = prev2 + abs(height[i]-height[i-2]);
+    
+      int cur_i=min(jumpOne, jumpTwo);
+      prev2=prev;
+      prev=cur_i;
+        
+  }
+  cout<<prev;
+}

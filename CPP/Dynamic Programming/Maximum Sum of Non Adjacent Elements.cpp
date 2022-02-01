5719813b854e0d5876233b982edef2b21a2ebbc0
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-02-01 14:59:22 +0530

    Create Maximum Sum of Non Adjacent Elements.cpp

---
+++ CPP/Dynamic Programming/Maximum Sum of Non Adjacent Elements.cpp
@@ -0,0 +1,106 @@
+// Memoization:
+// TC : O(N)
+// SC : O(N) + O(N)  
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solveUtil(int ind, vector<int>& arr, vector<int>& dp){
+    
+    if(dp[ind]!=-1) return dp[ind];
+    
+    if(ind==0) return arr[ind];
+    if(ind<0)  return 0;
+    
+    int pick= arr[ind]+ solveUtil(ind-2, arr,dp);
+    int nonPick = 0 + solveUtil(ind-1, arr, dp);
+    
+    return dp[ind]=max(pick, nonPick);
+}
+
+int solve(int n, vector<int>& arr){
+    vector<int> dp(n,-1);
+    return solveUtil(n-1, arr, dp);
+}
+
+
+int main() {
+
+  vector<int> arr{2,1,4,9};
+  int n=arr.size();
+  cout<<solve(n,arr);
+
+}
+
+// Tabulation:
+// TC : O(N)
+// SC : O(N)  
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int solveUtil(int n, vector<int>& arr, vector<int>& dp){
+    
+    dp[0]= arr[0];
+    
+    for(int i=1 ;i<n; i++){
+        int pick = arr[i];
+        if(i>1)
+            pick += dp[i-2];
+        int nonPick = 0+ dp[i-1];
+        
+        dp[i]= max(pick, nonPick);
+    }
+    
+    
+    return dp[n-1];
+}
+
+int solve(int n, vector<int>& arr){
+    vector<int> dp(n,-1);
+    return solveUtil(n, arr, dp);
+}
+
+
+int main() {
+
+  vector<int> arr{2,1,4,9};
+  int n=arr.size();
+  cout<<solve(n,arr);
+
+}
+
+// Space Optimization: 
+// TC : O(N)
+// SC : O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int n, vector<int>& arr){
+    int prev = arr[0];
+    int prev2 =0;
+    
+    for(int i=1; i<n; i++){
+        int pick = arr[i];
+        if(i>1)
+            pick += prev2;
+        int nonPick = 0 + prev;
+        
+        int cur_i = max(pick, nonPick);
+        prev2 = prev;
+        prev= cur_i;
+        
+    }
+    return prev;
+}
+
+
+int main() {
+
+  vector<int> arr{2,1,4,9};
+  int n=arr.size();
+  cout<<solve(n,arr);
+
+}

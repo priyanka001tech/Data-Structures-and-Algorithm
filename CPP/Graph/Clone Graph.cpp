2c149f42956f93b61d9f797048bab5a7d9307160
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-02-23 19:09:27 +0530

    Update Clone Graph.cpp

--- CPP/Graph/Clone Graph.cpp
+++ CPP/Graph/Clone Graph.cpp
@@ -1,3 +1,19 @@
+// Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+// Output: [[2,4],[1,3],[2,4],[1,3]]
+// Explanation: There are 4 nodes in the graph.
+// 1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+// 2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+// 3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+// 4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+
+//Input: adjList = [[]]
+//Output: [[]]
+
+//Input: adjList = []
+// Output: []
+// Explanation: This an empty graph, it does not have any nodes.
+
+
 class Solution {
 public:
     unordered_map<Node*, Node*> copies;

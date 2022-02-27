903d2c4eb83de2ad94007b2af2c0cef68c7b90d2
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-02-27 18:47:26 +0530

    Update Minimum Number of Steps to Make Two Strings Anagram II.cpp

--- CPP/HashMap/Minimum Number of Steps to Make Two Strings Anagram II.cpp
+++ CPP/HashMap/Minimum Number of Steps to Make Two Strings Anagram II.cpp
@@ -1,11 +1,11 @@
-# Minimum Number of Steps to Make Two Strings Anagram II
+// # Minimum Number of Steps to Make Two Strings Anagram II
 
 // You are given two strings s and t. In one step, you can append any character to either s or t.
 
 // Return the minimum number of steps to make s and t anagrams of each other.
 
 // An anagram of a string is a string that contains the same characters with a different (or the same) ordering.
-
+ 
 // Example 1:
 
 // Input: s = "leetcode", t = "coats" 

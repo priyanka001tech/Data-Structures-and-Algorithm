cd8eb84097b14c2894b1f65d8f2e51bef1801843
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-03-07 19:39:03 +0530

    Update Minimum Insertion Steps to Make a String Palindrome.cpp

--- CPP/Dynamic Programming/Minimum Insertion Steps to Make a String Palindrome.cpp
+++ CPP/Dynamic Programming/Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -1,3 +1,4 @@
+// Memoization Top-down
 class Solution {
 private:
     int LCS(string& s, string& t, int i,int j, vector<vector<int>>& dp){
@@ -22,4 +23,27 @@ public:
         return n-x;
     }
 };
- 
+
+// Tabulation bottom-up
+int minInsertions(string s) {
+        string t=s;
+        reverse(t.begin(), t.end());
+        int n=s.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+        for(int i=0;i<=n;i++) dp[i][0]=0;
+        for(int j=0;j<=n;j++) dp[0][j]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else{
+                   dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        //int x=LCS(s,t,n,n,dp);
+        int x=dp[n][n];  
+        return n-x;
+    }
+

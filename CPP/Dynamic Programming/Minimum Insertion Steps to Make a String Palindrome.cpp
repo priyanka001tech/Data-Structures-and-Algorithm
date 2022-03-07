6a6525e333e4bcd264630fd900a8815a117b9daa
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-03-07 19:28:11 +0530

    Update Minimum Insertion Steps to Make a String Palindrome.cpp

--- CPP/Dynamic Programming/Minimum Insertion Steps to Make a String Palindrome.cpp
+++ CPP/Dynamic Programming/Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -5,7 +5,7 @@ private:
         if(dp[i][j]!=-1) return dp[i][j];
         if(s[i-1]==t[j-1]){
             dp[i][j] = 1+LCS(s,t,i-1,j-1,dp);
-        } 
+        }  
         else{
            dp[i][j] = max(LCS(s,t,i,j-1,dp),LCS(s,t,i-1,j,dp));
         }

4b6e1b81d44c005343b646045b95a79cfcc0b4cf
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-03-30 19:45:44 +0530

    Update buy and stock with transaction fee VI.cpp

--- CPP/Dynamic Programming/buy and stock pattern/buy and stock with transaction fee VI.cpp
+++ CPP/Dynamic Programming/buy and stock pattern/buy and stock with transaction fee VI.cpp
@@ -80,3 +80,22 @@ public:
         return dp[0][1];
     }
 };
+
+//space optimization
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int n=prices.size();
+        vector<int> curr(2,0), next(2,0);
+        for(int idx=n-1; idx>=0; idx--){
+                    curr[1] = max(-prices[idx] + next[0],
+                              0 + next[1]);
+
+                    curr[0] = max(prices[idx]-fee + next[1],
+                              0 + next[0]);
+            next=curr;
+
+        }
+        return curr[1];
+    }
+};

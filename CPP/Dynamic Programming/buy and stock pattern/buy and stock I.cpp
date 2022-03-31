685bb6bcff676a5808b4e94589a63816be416299
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-03-31 19:31:10 +0530

    Update buy and stock I.cpp

--- CPP/Dynamic Programming/buy and stock pattern/buy and stock I.cpp
+++ CPP/Dynamic Programming/buy and stock pattern/buy and stock I.cpp
@@ -5,7 +5,7 @@
 // Example 1:
 
 // Input: prices = [7,1,5,3,6,4]
-// Output: 5
+// Output: 5 
 // Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 // Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 // Example 2:

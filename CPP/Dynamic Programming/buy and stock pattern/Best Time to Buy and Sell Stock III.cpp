3a140d7c4e46894f309e8f9d15ec16263c0662aa
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-04-01 18:45:49 +0530

    Update Best Time to Buy and Sell Stock III.cpp

--- CPP/Dynamic Programming/buy and stock pattern/Best Time to Buy and Sell Stock III.cpp
+++ CPP/Dynamic Programming/buy and stock pattern/Best Time to Buy and Sell Stock III.cpp
@@ -4,7 +4,7 @@
 
 // Example 1:
 
-// Input: prices = [3,3,5,0,0,3,1,4]
+// Input: prices = [3,3,5,0,0,3,1,4] 
 // Output: 6
 // Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
 // Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.

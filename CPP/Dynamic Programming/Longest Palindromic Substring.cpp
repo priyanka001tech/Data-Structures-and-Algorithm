58812e1a085504a860bc75469b7c3707786ada43
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-04-18 20:18:11 +0530

    Create Longest Palindromic Substring.cpp

---
+++ CPP/Dynamic Programming/Longest Palindromic Substring.cpp
@@ -0,0 +1,79 @@
+// ALGO:
+
+// Maintain a variable ‘ maxLength = 1 ‘ (for storing LPS length) and ‘ start =0 ‘ (for storing starting index of LPS ).
+
+// The idea is very simple, we will traverse through the entire string with i=0 to i<(length of string).
+
+// while traversing, initialize ‘low‘ and ‘high‘ pointer such that low= i-1 and high= i+1.
+
+// keep incrementing ‘high’ until str[high]==str[i] .
+
+// similarly keep decrementing ‘low’ until str[low]==str[i].
+
+// finally we will keep incrementing ‘high’ and decrementing ‘low’ until str[low]==str[high].
+
+// calculate length=high-low-1, if length > maxLength then maxLength = length and start = low+1 .
+
+// Print the LPS and return maxLength.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// A utility function to print
+// a substring str[low..high]
+// This function prints the
+// longest palindrome substring (LPS)
+// of str[]. It also returns the
+// length of the longest palindrome
+int longestPalSubstr(string str)
+{
+    int n = str.size(); // calculating size of string
+    if (n < 2)
+        return n; // if string is empty then size will be 0.
+                  // if n==1 then, answer will be 1(single
+                  // character will always palindrome)
+ 
+    int maxLength = 1,start=0;
+    int low, high;
+    for (int i = 0; i < n; i++) {
+        low = i - 1;
+        high = i + 1;
+        while ( high < n && str[high] == str[i]) //increment 'high'                                  
+            high++;
+       
+        while ( low >= 0 && str[low] == str[i]) // decrement 'low'                   
+            low--;
+       
+        while (low >= 0 && high < n && str[low] == str[high]){
+              low--;
+            high++;
+        }
+ 
+        int length = high - low - 1;
+        if (maxLength < length) {
+            maxLength = length;
+              start=low+1;
+        }
+    }
+     
+      cout<<"Longest palindrome substring is: ";
+      cout<<str.substr(start,maxLength);
+    return maxLength;
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    string str = "forgeeksskeegfor";
+    cout << "\nLength is: " << longestPalSubstr(str)
+         << endl;
+    return 0;
+}
+
+// Complexity Analysis:  
+
+// Time complexity: O(n^2), where n is the length of the input string. 
+// Outer Loop that traverse through entire string, and Inner Loop that is used to expend from i .
+// Auxiliary Space: O(1). 
+// No extra space is needed.

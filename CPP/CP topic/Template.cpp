e29ac81a70b6d2a717f21f0fefafc08ab11cb75e
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-04-30 12:38:42 +0530

    Create Template.cpp

---
+++ CPP/CP topic/Template.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll; // comments that are mixed in with code
+typedef pair<int, int> ii; // are aligned to the right like this
+typedef vector<ii> vii;
+typedef vector<int> vi;
+#define INF 1000000000
+
+// Common memset settings
+//memset(memo, -1, sizeof memo); // initialize DP memoization table with -1
+//memset(arr, 0, sizeof arr); // to clear array of integers
+
+// ans = a ? b : c; // to simplify: if (a) ans = b; else ans = c;
+// ans += val; // to simplify: ans = ans + val; and its variants
+// index = (index + 1) % n; // index++; if (index >= n) index = 0;
+// index = (index + n - 1) % n; // index--; if (index < 0) index = n - 1;
+// int ans = (int)((double)d + 0.5); // for rounding to nearest integer
+// ans = min(ans, new_computation); // min/max shortcut
+// alternative form but not used in this book: ans <?= new_computation;
+// some code use short circuit && (AND) and || (OR)
+
+int main(){
+     ios_base::sync_with_stdio(false);
+     cin.tie(NULL);
+     int t=1;
+     //cin >> t;
+     while(t--){
+        int n, s=0;
+        cin >> n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin >> arr[i];
+        }
+        
+    }
+    
+ }
